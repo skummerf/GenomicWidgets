90fe8dafce378681c4c58eaede99a144d7b018e9
finklej <finklej@ea8427e4-251f-0410-985a-fb3ea31d2ee2>
2016-07-05 16:15:51 +0000

    Remove example functions from trunk
    They cause errors during package development
    
    git-svn-id: http://resscm.gene.com/bioinfo/projects/Vishva/InternChIP/chipVis@193454 ea8427e4-251f-0410-985a-fb3ea31d2ee2

--- trunk/data/exampleDataSubset.R
+++
@@ -1,4 +0,0 @@
-clinicalData<-read.table(file="exampleData.txt", sep="\t", quote="")
-#Subset for asthma patients
-exampleDataSubset<-clinicalData[which(clinicalData$Disease == "Asthma"),]
-rm(clinicalData)
