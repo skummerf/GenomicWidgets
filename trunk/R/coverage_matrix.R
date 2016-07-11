9c4c5e04ef93896c32e2a66c3c410aa0ec35d1d0
schepa <schepa@ea8427e4-251f-0410-985a-fb3ea31d2ee2>
2016-07-11 16:44:32 +0000

    coverage matrix testing vignette
    git-svn-id: http://resscm.gene.com/bioinfo/projects/Vishva/InternChIP/chipVis@193793 ea8427e4-251f-0410-985a-fb3ea31d2ee2

--- trunk/R/coverage_matrix.R
+++ trunk/R/coverage_matrix.R
@@ -1,20 +1,39 @@
 #' make_coverage_matrix
 #' 
-#' @params inputs
-#' @params ranges
-#' @params binsize
-#' @params format
+#' @param inputs filenames of bigwig, bam, or RData file
+#' @param ranges ranges for which to compute coverage within
+#' @param binsize binsize to bin coverage
+#' @param format format of files, default is auto 
+#' @param up basepairs upstream of center to use
+#' @param down basepairs downstream of center to use
+#' @details up and down are 0 by default -- if not specified, actual range is used.  All ranges
+#' must be of equal width.  If up and/or down are provided, then the center of the range and up 
+#' basepairs upstream and down basepairs downstream are used.
+#' @import GenomicRanges
 #' @export
 make_coverage_matrix <- function(inputs, 
                                  ranges, 
                                  binsize = 1, 
                                  format = c("auto","bigwig","bam", "rle","RData"),
+                                 up = 0,
+                                 down = 0,
                                  ...){
   
   format = match.arg(format)
+  if (up > 0 || down > 0){
+    ranges <- resize(ranges, fix = "center", width = 1)
+    ranges <- promoters(ranges, upstream = up, downstream = down)
+  } 
+  
   stopifnot(sum(width(ranges) == width(ranges[1])) == length(ranges)) #check for equal widths
   if (binsize > 1 && width(ranges[1]) %% binsize != 0){
     ranges <- resize(ranges, fix = "center", width = (width(ranges[1]) %/% binsize +1)*binsize)
+  }
+  
+  if (up > 0 || down > 0){
+    coln <- seq(-up,down - binsize,binsize)
+  } else{
+    coln <- seq(1,width(ranges[1]),binsize)
   }
   
   #Determine format
@@ -36,40 +55,106 @@ make_coverage_matrix <- function(inputs,
         stop("Cannot determine format of inputs.")
       }
     } else{
-      stop("Cannot determine format of inputs. Rle method not yet implemented")
+      if (inherits(tmp,"RleList")){
+        format = "rle"
+      } else{
+        stop("Cannot determine format of inputs.")
+      }
     }
   }
   
   if (format == "bigwig"){
     # bw input
     if (length(inputs) == 1){
-      out <- coverage_mat_from_bigwig(inputs, ranges, binsize)
+      out <- coverage_mat_from_bigwig(inputs, ranges, binsize, coln)
     } else{
-      out <- BiocParallel::bplapply(inputs, coverage_mat_from_bigwig, ranges, binsize)
+      out <- BiocParallel::bplapply(inputs, coverage_mat_from_bigwig, ranges, binsize, coln)
     }
   } else if (format == "bam"){
     # bam input
     inputs = convert_to_full_path(inputs) #bamsignals seems to fail with symlinks
     #
     if (length(inputs) == 1){
-      out <- coverage_mat_from_bam(inputs, ranges, binsize)
+      out <- coverage_mat_from_bam(inputs, ranges, binsize, coln)
     } else{
-      out <- BiocParallel::bplapply(inputs, coverage_mat_from_bam, ranges, binsize)
+      out <- BiocParallel::bplapply(inputs, coverage_mat_from_bam, ranges, binsize, coln)
     }
   } else if (format == "RData"){
-    out <- getCvgList(ranges, inputs, flank = width(ranges[1])%/%2, normalize.method = "none", bins = binsize, ...)
+    out <- getCvgList(ranges, inputs, flank = 0, normalize.method = "none", bins = binsize, ...)
+    # if (length(inputs) == 1){
+    #   out <- out[[1]]
+    #   colnames(out) = coln
+    # } else{
+    #   out <- lapply(out, function(x) {colnames(x) = coln; return(x)})
+    # }
   } else if (format == "rle"){
-    # rle input 
-    stop("not yet implemented")
+    if (length(inputs) == 1){
+      out <- getBinnedCvg(ranges, inputs, bins = binsize, flank = 0)
+      #colnames(out) = coln
+    } else{
+      out <- lapply(inputs, function(x) getBinnedCvg(ranges, x, bins = binsize, flank = 0))
+      #out <- lapply(out, function(x) {colnames(x) = coln; return(x)})
+    }
   } else {
     stop("auto method not yet implemented")
+  }
+  
+  return(out)
+}
+
+#' normalize_coverage_matrix
+#' 
+#' @param mats
+#' @param 
+#' @export
+normalize_coverage_matrix <- function(mats, 
+                                      method = c("localRms", "localMean", 
+                                                      "localNonZeroMean", "PercentileMax", "scalar", "none"), 
+                                      pct = 0.95, scalar = NULL){
+  
+  if (length(mats) == 1){
+    out <- normalize_coverage_matrix_single(mats, method, pct, scalar)
+  } else{
+    out <-BiocParallel::bplapply(mats, normalize_coverage_matrix_single, method, pct, scalar)
   }
   return(out)
 }
 
-### TO DO:  Normalize coverage matrix 
 
-
+## From gChipseq package
+normalize_coverage_matrix_single <- function(mat, method = c("localRms", "localMean", 
+                                     "localNonZeroMean", "PercentileMax", "scalar", "none"), 
+          pct = 0.95, scalar = NULL) 
+{
+  method <- match.arg(method)
+  if (method == "localRms") {
+    scaler <- sqrt(rowSums(mat^2))
+    scaler[scaler == 0] <- 1
+    return(mat/scaler)
+  } else if (method == "localMean") {
+    scaler <- rowMeans(mat)
+    scaler[scaler == 0] <- 1
+    return(mat/scaler)
+  } else if (method == "localNonZeroMean") {
+    scaler <- apply(mat, 1, function(x) {
+      mean(x[x != 0])
+    })
+    scaler[is.nan(scaler)] <- 1
+    return(mat/scaler)
+  } else if (method == "PercentileMax") {
+    scaler <- quantile(mat, pct)
+    if (scaler == 0) {
+      scaler <- 1
+    }
+    res <- mat/scaler
+    res[res[] > 1] <- 1
+    return(res)
+  } else if (method == "scalar") {
+    return(mat/scalar)
+  } else if (method == "none") {
+    return(mat)
+  }
+}
 
 
 
@@ -85,7 +170,7 @@ bin_mat <- function(mat, binsize){
 }
 
 
-coverage_mat_from_bigwig <- function(bigwig_file, ranges, binsize){
+coverage_mat_from_bigwig <- function(bigwig_file, ranges, binsize, coln){
   stopifnot(sum(width(ranges) == width(ranges[1])) == length(ranges)) #check for equal widths
   tmp = rtracklayer::import.bw(bigwig_file, selection = ranges, as = "NumericList")
   tmp_list = lapply(1:length(ranges), function(x){
@@ -97,36 +182,65 @@ coverage_mat_from_bigwig <- function(bigwig_file, ranges, binsize){
   }) 
   tmp_mat = t(simplify2array(tmp_list))
   if (binsize == 1){
+    colnames(tmp_mat) = coln
     return(tmp_mat)
   } else{
-    return(bin_mat(tmp_mat, binsize))
+    binned <- bin_mat(tmp_mat, binsize)
+    colnames(binned) <- coln
+    return(binned)
   }
 }
 
 
 ### This does not do read extension -- to do that you would need to use bamProfile and then smooth with
 ### flat window.  
-coverage_mat_from_bam <- function(bam_file, ranges, binsize, ...){
+coverage_mat_from_bam <- function(bam_file, ranges, binsize, coln){
   stopifnot(sum(width(ranges) == width(ranges[1])) == length(ranges)) #check for equal widths
   cvg <- bamsignals::bamCoverage(bam_file, ranges)
   tmp_mat <- t(bamsignals::alignSignals(cvg))
   if (binsize == 1){
+    colnames(tmp_mat) = coln
     return(tmp_mat)
   } else{
-    return(bin_mat(tmp_mat, binsize))
+    binned <- bin_mat(tmp_mat, binsize)
+    colnames(binned) <- coln
+    return(binned)
   }
 }
 
-coverage_mat_from_rle <- function(rle, ranges, binsize, ...){
-  stopifnot(sum(width(ranges) == width(ranges[1])) == length(ranges)) #check for equal widths
-  cvg <- 
-    
-  if (binsize == 1){
-    return(tmp_mat)
-  } else{
-    return(bin_mat(tmp_mat, binsize))
+
+#Function from Johannes Helmuth, https://github.com/lamortenera/bamsignals/issues/16
+bamCoverageFrags <- function(bampath, gr, fragLength=200, ss = TRUE, ...) {
+  prf <- bamsignals::bamProfile(bampath, gr, binsize=1, ss = TRUE, ...)
+  cov <- lapply(as.list(prf), function(p) {
+    l <- dim(p)[2]
+    x1 <- cumsum(as.numeric(p[1,]))
+    y1 <- c(rep(0, fragLength), x1[1:(l-200)])
+    x2 <- cumsum(as.numeric(rev(p[2,])))
+    y2 <- c(rep(0, fragLength), x2[1:(l-200)])
+    matrix(c(x1-y1, rev(x2-y2)), ncol = l, byrow = T,
+           dimnames = list(c("sense", "antisense"), NULL))
+  })
+  if (ss == TRUE) {
+    prf@signals <- cov
+  } else {
+    prf@signals <- lapply(cov, colSums)
   }
+  invisible(prf)
 }
+
+
+
+# coverage_mat_from_rle <- function(rle, ranges, binsize, ...){
+#   stopifnot(sum(width(ranges) == width(ranges[1])) == length(ranges)) #check for equal widths
+#   cvg <- 
+#     
+#   if (binsize == 1){
+#     return(tmp_mat)
+#   } else{
+#     return(bin_mat(tmp_mat, binsize))
+#   }
+# }
 
 convert_to_full_path <- function(x){
   for (i in 1:length(x)){
@@ -137,16 +251,15 @@ convert_to_full_path <- function(x){
 
 
 
-# 
-# 
-# #Is this just the same as matrix, but with one?
-# get_coverage_track <- function(inputs, binsize){
-#   
-#   # bw input
-#   
-#   # bam input
-#   
-#   # rdata input
-#   
-#   
-# }
+
+
+#Is this just the same as matrix, but with one?
+make_coverage_tracks <- function(inputs, ranges, 
+                               binsize = 1, 
+                               format = c("auto","bigwig","bam", "rle","RData"), ...){
+
+  stopifnot(length(ranges) == 1)
+  out <- make_coverage_matrix(inputs, ranges, binsize, format,...)
+  out <- do.call(rbind,out)
+  return(out)
+}
