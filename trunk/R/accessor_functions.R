1226b8c56587391b2e2308f45b3e9abebd0ee701
finklej <finklej@ea8427e4-251f-0410-985a-fb3ea31d2ee2>
2016-07-12 00:07:28 +0000

    Basic accessor functions added
    git-svn-id: http://resscm.gene.com/bioinfo/projects/Vishva/InternChIP/chipVis@193866 ea8427e4-251f-0410-985a-fb3ea31d2ee2

--- trunk/R/accessor_functions.R
+++ trunk/R/accessor_functions.R
@@ -6,7 +6,7 @@
 #' @param samples_file string
 #' @param pairs_file string
 #' @return list
-#' @export
+#' @import gChipseq
 #' @author Justin Finkle
 #' @examples
 getChipFileInfo <- function(samples_file, pairs_file){
@@ -21,3 +21,77 @@ getChipFileInfo <- function(samples_file, pairs_file){
   
   return(full_info)
 }
+
+#' Title
+#'
+#' @param file_info 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+getSampleNames <- function(file_info){
+  return(file_info[["Sample.Name"]])
+}
+
+#' Title
+#'
+#' @param file_info 
+#' @param name.vector 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+addShortNames <- function(file_info, name.vector){
+  file_info <- data.frame(append(file_info, list("Short.Name"=name.vector), 
+                                 after=match("Sample.Name", names(file_info))))
+  return(file_info)
+}
+
+#' Title
+#'
+#' @param file_info 
+#' @param es_identifier 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+addExpressionIdentifier <- function(file_info, es_identifier){
+  file_info[['ExPlot.proj']] <- es_identifier
+  return(file_info)
+}
+
+#' Title
+#'
+#' @param proj 
+#' @import ExpressionPlot
+#' @return
+#' @export
+#'
+#' @examples
+getExpressionSetRPKM <- function(proj){
+  proj = ep.find.project(proj)
+  esetRPKM<- ep.ExpressionSet(proj = proj, feature.type = "gene", stat = "rpkm",
+                              attach.annot = TRUE)
+  return(esetRPKM)
+  
+}
+
+getControlSample <- function(sample, file_info){
+  input_name <- file_info[file_info[["Sample.Name"]] == sample, "Sample.Control"]
+  return(input_name)
+}
+
+checkFiles <- function(df){
+  sapply(grep("^File", colnames(df), value = TRUE),
+         function(fileColumn) file.exists(df[, fileColumn])
+  )
+}
+
+getSampleInfo <- function( sample, file_info){
+  sample_info <- file_info[file_info[["Sample.Name"]] == sample, ]
+  return(as.list(sample_info))
+}
+
--- trunk/tests/testthat/test_load_files.R
+++ trunk/tests/testthat/test_load_files.R
@@ -2,8 +2,8 @@ context('File Access')
 
 test_that("getChipFileInfo loads proper dataframe", {
   samples <- "../testdata/test_samples.txt"
-  pairs <- "../testdata/test_paris.txt"
+  pairs <- "../testdata/test_pairs.txt"
   file_info <- getChipFileInfo(samples, pairs)
-  expect_is(file_info, "data.frame"
-  rm(samples, pairs)
+  expect_equal_to_reference(file_info, "../testdata/test_file_info.rds")
+  rm(samples, pairs, file_info)
 })
