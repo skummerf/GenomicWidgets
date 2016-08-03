7398851dafc9d0a6731586a8e49921f42a3e09cf
schepa <schepa@ea8427e4-251f-0410-985a-fb3ea31d2ee2>
2016-08-03 16:19:51 +0000

    intial commit of generic shiny app for heatmaps plus tracks
    git-svn-id: http://resscm.gene.com/bioinfo/projects/Vishva/InternChIP/chipVis@195670 ea8427e4-251f-0410-985a-fb3ea31d2ee2

---
+++ trunk/R/heatmap_plus_tracks_shiny.R
@@ -0,0 +1,72 @@
+
+HEATMAP_SOURCE = "HM"
+
+heatmap_click <- function(heatmap, ranges, width = 50000){
+  ranges <- resize(ranges, fix = "center", width = width)
+  out <- function(){
+    s <- event_data("plotly_click", source = HEATMAP_SOURCE)
+    if(is.null(s) == T) return(NULL)
+    if (heatmap$curve_map[[s$curve + 1]]$yaxis != "yaxis") return(NULL)
+    ix <- heatmap$row_order[s$y + 1]
+    return(ranges[ix])
+  }
+  return(out)
+}
+
+tracks_placeholder <- function(cvg_files, genome, org, scaling_factor, ...){
+  out <- function(range){
+    if (is.null(range)) return(NULL)
+    de <- get_exons(range, genome = genome, org = org)
+    grl <- get_coverage_in_range(cvg_files, 
+                                 range, 
+                                 names = names(cvg_files), 
+                                 scaling.factor = scaling_factor)
+    plot_track_view(cvg.L =  grl, 
+                    target.range = range,
+                    transcripts.gr = de,
+                    genome = genome, ...)
+  }
+  return(out)
+}
+
+heatmap_to_tracks_shiny <- function(heatmap, 
+                                    track_function,
+                                    link,
+                                    title = "Heatmap linked to Genome Tracks",
+                                    options = list(height = 1400)){
+  require(shiny)
+  require(plotly)  
+  
+  # Check regions
+  
+  ui <- fluidPage(
+    
+    # Application title
+    titlePanel(title),
+    
+    fluidRow(
+      plotlyOutput("heat")
+    ),
+    fluidRow(
+      plotOutput("tracks")
+    )
+  )
+  
+  # Define server logic required to draw a histogram
+  server <- function(input, output) {
+    
+    output$heat <- renderPlotly({
+      heatmap %>% plot_iHeatmap(source = HEATMAP_SOURCE)
+    })
+    
+    output$tracks <- renderPlot({
+      linker <- link() 
+      track_function(linker)  
+    })
+    
+  }
+  
+  # Run the application 
+  shinyApp(ui = ui, server = server, options = options)
+  
+}
