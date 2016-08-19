74f556838740561499fe15ca7c522e1fd4ff0299
finklej <finklej@ea8427e4-251f-0410-985a-fb3ea31d2ee2>
2016-08-19 20:56:51 +0000

    first commit, prototype of log-scale chip statistics
    git-svn-id: http://resscm.gene.com/bioinfo/projects/Vishva/InternChIP/chipVis@196628 ea8427e4-251f-0410-985a-fb3ea31d2ee2

--- trunk/R/feature_extraction.R
+++ trunk/R/feature_extraction.R
@@ -15,9 +15,10 @@ avg_peak_in_range <- function(gr, peaks_gr){
   peaks_per_range <- splitAsList(mcols(peaks_summit)$score[queryHits(overlaps)],
                                  factor(subjectHits(overlaps)))
   
-  # Need a better way to scale the data
+  # Average peaks in each bin
   range_score <- lapply(peaks_per_range, mean)
-  mcols(gr)[['avg_peak']] <- unlist(range_score)
+  mcols(gr)[['avg_peak']] <- 0
+  mcols(gr)[['avg_peak']][as.numeric(names(range_score))] <- unlist(range_score)
   return(gr)
 }
   
@@ -32,26 +33,39 @@ avg_peak_in_range <- function(gr, peaks_gr){
 #' @export
 #'
 #' @examples
-log_scale_range <- function(gr, n = 10){
-  # Split the range in two
-  split_range <- tile(x = gr, n =2)[[1]]
+log_scale_range <- function(center,
+                            chromosome,
+                            n = 10, 
+                            extension = 1000000, 
+                            min_bin = 100){
+  
+  new_gr <- GRanges(seqnames = chromosome,
+                    ranges = IRanges(center-extension, width=2*extension))
+  split_range <- tile(x = new_gr, n = 2)[[1]]
   
   # Tile each half recursively
   # The left range is recurisively tiled on the right side, so that the granges
-  left_range <- recursive_tile_range(split_range[1], side = "right", n = n)
-  right_range <- recursive_tile_range(split_range[2], side = "left", n = n)
+  left_range <- recursive_tile_range(split_range[1], 
+                                     side = "right", 
+                                     n = n, 
+                                     min_bin = min_bin)
+  right_range <- recursive_tile_range(split_range[2], 
+                                      side = "left", 
+                                      n = n,
+                                      min_bin = min_bin)
   return(c(left_range, right_range))
 }
 
 recursive_tile_range <- function(gr, 
-                            side = c('right', 'left'), 
-                            n = 10){
+                                 side = c('right', 'left'), 
+                                 n = 10,
+                                 min_bin = 100){
   if(n <=1){
     stop("invalid n for recursive tiling. n must be > 1")
   }
   side <- match.arg(side)
   # If the range isn't wide enough to split into n bins, you're at the bottom
-  if(width(gr)<n){
+  if(width(gr)<n | width(gr)< min_bin*n){
     return(gr)
   } else {
     # Split into n bins
@@ -60,11 +74,17 @@ recursive_tile_range <- function(gr,
     # Call recursion on appropriate side
     if(side == 'right'){
       left_range <- tiled_range[1:(n-1)]
-      right_range <- recursive_tile_range(tiled_range[n], side = side, n = n)
+      right_range <- recursive_tile_range(tiled_range[n], 
+                                          side = side, 
+                                          n = n,
+                                          min_bin = min_bin)
       
     } else if(side == 'left'){
       right_range <- tiled_range[2:n]
-      left_range <- recursive_tile_range(tiled_range[1], side = side, n = n)
+      left_range <- recursive_tile_range(tiled_range[1], 
+                                         side = side, 
+                                         n = n,
+                                         min_bin = min_bin)
     }
     return(c(left_range, right_range))
   }
