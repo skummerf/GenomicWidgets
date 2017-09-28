50cf4b31c4d3691c99804f6feff0ac0d0ff56642
AliciaSchep <aschep@gmail.com>
2017-09-28 16:07:04 -0700

    add test for reading in bigwig

--- tests/testthat/test_coverage_mat.R
+++ tests/testthat/test_coverage_mat.R
@@ -1,5 +1,11 @@
 context("Coverage matrix")
 
+
+geno_mats <- genomation::ScoreMatrixList(targets = samp.info$fileName[1:3],
+                                           windows = resize(ctcf.peaks[1:10], 
+                                                            width = 500, 
+                                                            fix = "start"), 
+                                           bin.num = 20)
 test_that("Coverage matrix works with bam file",{
   
   test_mats <- make_coverage_matrix(samp.info$fileName[1:3], 
@@ -9,11 +15,22 @@ test_that("Coverage matrix works with bam file",{
                                     down = 250, 
                                     binsize = 25)
   
-  geno_mats <- genomation::ScoreMatrixList(targets = samp.info$fileName[1:3],
-                                           windows = resize(ctcf.peaks[1:10], 
-                                                            width = 500, 
-                                                            fix = "start"), 
-                                           bin.num = 20)
+  expect_is(test_mats, "SummarizedExperiment")
+  
+  expect_equivalent(assays(test_mats)[[1]], geno_mats[[1]]@.Data)
+  expect_equivalent(assays(test_mats)[[2]], geno_mats[[2]]@.Data)
+  expect_equivalent(assays(test_mats)[[3]], geno_mats[[3]]@.Data)
+  
+})
+
+test_that("Coverage matrix works with bigwig file",{
+  
+  test_mats <- make_coverage_matrix(c("bw1.bw","bw2.bw","bw3.bw"), 
+                                    ctcf.peaks[1:10], 
+                                    input_names = samp.info$sampleName[1:3],
+                                    up = 250, 
+                                    down = 250, 
+                                    binsize = 25)
   
   expect_is(test_mats, "SummarizedExperiment")
   
@@ -22,4 +39,5 @@ test_that("Coverage matrix works with bam file",{
   expect_equivalent(assays(test_mats)[[3]], geno_mats[[3]]@.Data)
   
 })
+
 
